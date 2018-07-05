92b5167e835ff79d76be617348121fa22b422421
Tim Bishop <tim.bish61@gmail.com>
2018-07-05 16:53:19 +0100

    testingthe 2nd level

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\timbi\source\repos\DJ05072018\DJ05072018\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
